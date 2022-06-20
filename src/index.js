556890c4843edaed29747efe957d01f20ca87dd2
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-20 09:06:31 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener( 'DOMContentLoaded', () =>{
+    fetch( "https://dog.ceo/api/breeds/image/random/4" )
+      .then( ( res ) => res.json() )
+      .then( ( data ) =>
+      {
+        let array = data.message;
+        for ( const item of array )
+        {
+          let card = document.querySelector( '#dog-image-container' )
+          let img = document.createElement( 'img' )
+          img.setAttribute( 'src', item )
+          card.appendChild( img );
+        }
+      } );
+    
+  } );
+  
+  function findDogByBreed(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+  }
